56191560cd939dd05aae328b79955fb1d4b55210
omgitsdawise <naseerabood@gmail.com>
2020-01-06 14:49:24 +0200

    Changed Login/Register UI, and added edge cases.

--- app/src/main/java/com/lemonlab/all_in_one/LoginFragment.kt
+++ app/src/main/java/com/lemonlab/all_in_one/LoginFragment.kt
@@ -2,18 +2,23 @@ package com.lemonlab.all_in_one
 
 
 import android.os.Bundle
-import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.lemonlab.all_in_one.extensions.highlightText
 import com.lemonlab.all_in_one.extensions.navigateToAndClear
-import kotlinx.android.synthetic.main.fragment_login.view.*
+import com.lemonlab.all_in_one.extensions.showMessage
+import com.lemonlab.all_in_one.items.CategoryPics
+import com.lemonlab.all_in_one.items.categories
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlin.random.Random
 
 /**
- * A simple [Fragment] subclass.
+ * A fragment users can login from.
  */
 
 
@@ -28,31 +33,69 @@ class LoginFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        init()
         super.onViewCreated(view, savedInstanceState)
 
-        view.log_in_btn.setOnClickListener {
-            val email = view.useremail_edittext.text.toString()
-            val password = view.user_password_edittext.text.toString()
-            logInUser(email = email, password = password)
-        }
-
-        view.sign_up_tv.setOnClickListener {
-            view.navigateToAndClear(R.id.loginFragment, R.id.registerFragment)
-        }
     }
 
-    private fun logInUser(email:String, password:String){
+    private fun init() {
+        login_quote_text_tv.text = context!!.highlightText(getRandomQuote())
+        login_text_image.setImageResource(CategoryPics.getRandomPic())
 
-        // check if email pr password is empty or invalid
-        if(email.isEmpty() || password.isEmpty()){
-            Toast.makeText(context!!, resources.getString(R.string.invalidEmailPassword), Toast.LENGTH_LONG).show()
-            return
+        login_btn.setOnClickListener {
+            val email = login_user_email_edit_text.text.toString()
+            val password = login_user_password_edit_text.text.toString()
+            // check if email or password is empty/invalid
+            checkFields(email, password)
         }
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnSuccessListener{
-            view!!.navigateToAndClear(R.id.loginFragment, R.id.mainFragment)
-        }.addOnFailureListener{
-
+        register_tv.setOnClickListener {
+            it.findNavController().popBackStack()
+            it.findNavController().navigate(R.id.registerFragment)
         }
+
+    }
+
+    private fun getRandomQuote(): String {
+        val list = LocalQuotesFragment().getStatuses(
+            categories[Random.nextInt(categories.size)],
+            resources
+        )
+        return list[Random.nextInt(list.size)]
+    }
+
+
+    private fun checkFields(userEmail: String, userPassword: String) {
+
+        val emailOK = if (userEmail.contains('@'))
+            Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()
+        else
+            false
+
+        val passwordOK = userPassword.length >= 6
+        if (!emailOK)
+            login_user_email_edit_text.error = getString(R.string.invalidEmail)
+        if (!passwordOK)
+            login_user_password_edit_text.error = getString(R.string.invalidPassword)
+
+        if (emailOK && passwordOK)
+            login(email = userEmail, password = userPassword)
+
+    }
+
+    private fun login(email: String, password: String) {
+        context!!.showMessage(getString(R.string.signing_in))
+        login_btn.isEnabled = false
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                view!!.navigateToAndClear(R.id.loginFragment, R.id.mainFragment)
+            }.addOnFailureListener {
+                if (it.localizedMessage!!.contains("no user"))
+                    context!!.showMessage(getString(R.string.no_user))
+                else
+                    context!!.showMessage(getString(R.string.problem_occurred))
+                login_btn.isEnabled = true
+            }
     }
 }
--- app/src/main/java/com/lemonlab/all_in_one/RegisterFragment.kt
+++ app/src/main/java/com/lemonlab/all_in_one/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.lemonlab.all_in_one
 
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,13 @@ import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.lemonlab.all_in_one.extensions.navigateToAndClear
+import com.lemonlab.all_in_one.extensions.removeWhitespace
 import com.lemonlab.all_in_one.extensions.showMessage
 import com.lemonlab.all_in_one.model.User
-import kotlinx.android.synthetic.main.fragment_register.view.*
+import kotlinx.android.synthetic.main.fragment_register.*
 
 /**
- * A simple [Fragment] subclass.
+ * A fragment users where can create an account.
  */
 class RegisterFragment : Fragment() {
 
@@ -27,38 +29,71 @@ class RegisterFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        init()
         super.onViewCreated(view, savedInstanceState)
 
-        // register a new user in FireBase and store its information in firestore
-        view.register_btn.setOnClickListener{
-            val email = view.useremail_edittext.text.toString()
-            val password = view.user_password_edittext.text.toString()
-            val username = view.username_edit_text.text.toString()
-            registerNewUser(email = email, password = password, name = username)
+    }
+
+
+    private fun init() {
+
+        // register a new user in FireBase and store its information in fireStore
+        register_btn.setOnClickListener {
+            val email = user_email_edit_text.text.toString().removeWhitespace()
+            val password = user_password_edit_text.text.toString().removeWhitespace()
+            val username = username_edit_text.text.toString().removeWhitespace()
+            checkFields(email, password, username)
         }
 
         // navigate to log in fragments
-        view.log_in_tv.setOnClickListener {
-            view.navigateToAndClear(R.id.registerFragment, R.id.loginFragment)
+        log_in_tv.setOnClickListener {
+            view!!.navigateToAndClear(R.id.registerFragment, R.id.loginFragment)
         }
 
     }
 
-    private fun registerNewUser(email: String, password: String, name:String){
+    private fun checkFields(userEmail: String, userPassword: String, username: String) {
+
+        val emailOK = if (userEmail.contains('@'))
+            Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()
+        else
+            false
+
+        val passwordOK = userPassword.length >= 6
+
+        val usernameOK = username.length > 5
+
+        if (!emailOK)
+            user_email_edit_text.error = getString(R.string.invalidEmail)
+
+        if (!passwordOK)
+            user_password_edit_text.error = getString(R.string.invalidPassword)
+
+        if (!usernameOK)
+            username_edit_text.error = getString(R.string.invalidUsername)
+
+        if (emailOK && passwordOK && usernameOK)
+            registerNewUser(email = userEmail, password = userPassword, name = username)
+
+    }
+
+    private fun registerNewUser(email: String, password: String, name: String) {
         val user = User(name = name, email = email, online = true)
+
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener{
+            .addOnSuccessListener {
                 addUserToFireStore(user)
-                view!!.navigateToAndClear(R.id.registerFragment, R.id.mainFragment)
-            }.addOnFailureListener{
-                context!!.showMessage("حدثت مشكلة اثناء الاتصال")
+            }.addOnFailureListener {
+                context!!.showMessage(getString(R.string.problem_occurred))
             }
+
     }
 
     private fun addUserToFireStore(user: User) {
         val db = FirebaseFirestore.getInstance()
         val uid = FirebaseAuth.getInstance().uid
         db.collection("users").document("$uid").set(user)
+        view!!.navigateToAndClear(R.id.registerFragment, R.id.mainFragment)
     }
 
 }
